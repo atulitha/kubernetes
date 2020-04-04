51ca7059eee1e857ba92a7b5a5d11173d6813a47
atulitha-revanth <pandu.revanth@gmail.com>
2020-04-04 16:30:20 +0530

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,25 @@
+#Generating RSA Keys
+#The first step involves creating a set of RSA keys for use in authentication.
+#This should be done on the client.
+#To create your public and private SSH keys on the command-line:
+
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+ssh-keygen -t rsa
+
+#ssh-keygen -t rsa -b 4096
+#copy keys to ssh server
+#ssh-copy-id <username>@<host>
+ssh-copy-id node1@node
+
+#/etc/motd (Message of the Day)
+#/etc/ssh/sshd_config: Change the setting PrintLastLog to "no",
+#this will disable the "Last login" message.
+
+#nano /etc/motd
+
+#nano /etc/ssh/sshd_config
+
+#ls /etc/update-motd.d/
+
+#nano /var/run/motd
