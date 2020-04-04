40a397cf4c211a7a2a688d278434edc614ded745
atulitha-revanth <pandu.revanth@gmail.com>
2020-04-04 15:47:26 +0530

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#update and installing necessary update
+sudo apt-get update -y && sudo apt-get upgrade -y ; sudo apt autoremove -y
+
+# install docker
+sudo apt-get install docker.io curl -y
+
+#check docker version
+docker --version
+
+#setting docker to auto start
+sudo systemctl enable docker
